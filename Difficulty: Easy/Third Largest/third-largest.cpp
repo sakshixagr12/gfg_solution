66634937a1b929d6cb481c8486cbb82caae13388
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-08 18:25:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Third Largest/third-largest.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int thirdLargest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        if(n<3) return -1;
+        
+        sort(arr.begin(),arr.end());
+        return arr[n-3];
+    }
+};
