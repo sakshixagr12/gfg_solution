dfafab6cbc0bba823555453172c40eccff64add3
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-18 15:36:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Reverse Spiral Form of Matrix/reverse-spiral-form-of-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    vector<int> reverseSpiral(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size(); //rows
+        int m = mat[0].size(); // column
+        int top=0;
+        int left=0;
+        int right=m-1;
+        int bottom=n-1;
+        vector<int>answer;
+        
+        while(top<=bottom && left<=right){
+            for(int i = left ;i<=right ;i++) answer.push_back(mat[top][i]);
+            top++;
+            for(int i = top ; i<=bottom;i++) answer.push_back(mat[i][right]);
+            right--;
+            if(top<=bottom) {
+                for(int i = right ; i>=left;i--) answer.push_back(mat[bottom][i]);
+                bottom--;
+            }
+            if(left<=right){
+                for(int i = bottom;i>=top;i--) answer.push_back(mat[i][left]);
+                left++;
+            }
+        }
+        reverse(answer.begin(),answer.end());
+        return answer;
+    }
+};
