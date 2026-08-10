153a5ffa3946a97bb5ea320c00e118b232112d02
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-08-10 19:14:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Non-Leaf Nodes in Tree/count-non-leaf-nodes-in-tree.cpp
@@ -0,0 +1,20 @@
+/*Structure of the node of the tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+// you are required to complete this function
+// the function should return the count of Non-Leaf Nodes
+class Solution {
+  public:
+    int countNonLeafNodes(Node* root) {
+        // Code here
+        if(root==nullptr) return 0;
+        if(root->left||root->right)
+        return 1+countNonLeafNodes(root->left)+countNonLeafNodes(root->right);
+        return 0;
+    }
+};
