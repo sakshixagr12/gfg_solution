13e6974e60ec8b0e3cf5d64fe7af5d20ddea0d13
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-08-21 01:58:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.cpp
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -3,9 +3,9 @@ class Solution {
     void rotateArr(vector<int>& arr, int d) {
         // code here
         int n=arr.size();
-         d = d%n;
+        d%=n;
         reverse(arr.begin(),arr.begin()+d);
-        reverse(arr.begin()+d,arr.end());
+        reverse(arr.begin()+d,arr.end());\
         reverse(arr.begin(),arr.end());
     }
 };
